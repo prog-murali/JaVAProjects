6e9efffa96c354e075a774310ac997617b74915a
Murali Krishnaa <82220261+prog-murali@users.noreply.github.com>
2023-07-02 18:55:04 +0530

    Create READM.Calci.java
    Refer Before using Calci.java

---
+++ READM.Calci.java
@@ -0,0 +1,21 @@
+# Simple Calci Application
+
+This is a simple calculator application written in Java. It allows users to perform basic arithmetic operations, such as addition, subtraction, multiplication, division, and modulus.
+
+## Topics Covered
+
+* Input and output
+*Basic arithmetic operations
+* Exception handling
+* Loops
+* Conditional statements
+
+## How to use
+
+To use the application, simply run the `Calci.java` file. You will be prompted to enter two numbers. After you have entered the numbers, you will be prompted to select an operation. The application will then perform the operation and print the result.
+
+## Exception handling
+
+The application handles the `java.util.InputMismatchException` exception. This exception is thrown if the user enters an invalid input. The application will print a message to the user and then ask them to enter a valid input.
+
+  I hope this is helpful! Let me know if you have any other questions.
